162ba2a8215673bd8529390e73cd6b874cde8c32
Georgina Kidger <georgina.kidger@hotmail.com>
2024-12-19 01:25:07 +0000

    created run.py and base.html as template for application + workout database in postgres

---
+++ workouts/routes.py
@@ -0,0 +1,7 @@
+from flask import render_template
+from workouts import app, db
+from workouts.models import Category, Exercise
+
+@app.route("/")
+def home():
+    return render_template("base.html")
